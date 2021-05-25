f468671d6d97fe4a24a6ce76edef36b32e9831c3
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-25 17:04:19 +0200

    app-first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container text-center mt-3">
+      <h1>Vue Dischi</h1>
+
+      <div class="row">
+        <Song v-for="(song, index) in songs" :key="index" :song="song" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Song from "./components/Song.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Song,
+  },
+
+  data() {
+    return {
+      axios,
+      songs: [],
+    };
+  },
+
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.songs = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
 
 <style lang="scss">
+@import "@/assets/style/general.scss";
 </style>
