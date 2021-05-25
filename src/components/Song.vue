08fa6fb723ba53e4f91222366a0b24d9862bf77f
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-25 21:15:08 +0200

    song-first

--- src/components/Song.vue
+++ src/components/Song.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-sm-6 col-md-4 col-lg-3">
+  <div class="col-sm-6 col-md-4 col-lg-2">
     <img :src="song.poster" alt="" />
     <h2>{{ song.title }}</h2>
     <h4>{{ song.author }}</h4>
@@ -16,8 +16,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-img {
-  width: 150px;
-  height: 150px;
+div {
+  border: solid 8px rgb(30, 45, 59);
+  background-color: rgb(46, 58, 70);
+
+  img {
+    width: 150px;
+    height: 150px;
+    padding: 5px;
+  }
+
+  h2 {
+    font-size: 20px;
+    padding-bottom: 5px;
+    padding-top: 5px;
+    color: white;
+  }
+
+  h4 {
+    font-size: 15px;
+    color: gray;
+  }
 }
 </style>
