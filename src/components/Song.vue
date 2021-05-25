70c231764e610d58550c514a6f546398ac0b9ce4
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-25 17:03:48 +0200

    Song-first

---
+++ src/components/Song.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <img :src="song.poster" alt="" />
+    <h2>{{ song.title }}</h2>
+    <h4>{{ song.author }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Song",
+  props: {
+    song: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 150px;
+  height: 150px;
+}
+</style>
